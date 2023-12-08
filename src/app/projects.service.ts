c1fe180aad9e57e177f6a2697dfacefced74a09e
Diego Rodriguez <diego25598@gmail.com>
2023-12-09 00:18:37 +0100

    add projects service

--- src/app/projects.service.ts
+++ src/app/projects.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
+
 export class ProjectsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getProjects(): Observable<any> {
+    const url = 'http://localhost:30030/fundme/controller/rest/projects/all';
+    const headers = new HttpHeaders();
+    return this.http.get<any>(url, { headers });
+  }
 }
