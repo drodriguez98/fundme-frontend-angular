596715c4e5eaa687e9aae36dec702825272a8247
Diego Rodriguez <diego25598@gmail.com>
2023-12-09 04:30:25 +0100

    update donation component

--- src/app/donation-home/donation-home.component.ts
+++ src/app/donation-home/donation-home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { DonationsService } from '../donations.service';
 
 @Component({
   selector: 'app-donation-home',
   templateUrl: './donation-home.component.html',
   styleUrls: ['./donation-home.component.css']
 })
-export class DonationHomeComponent implements OnInit {
 
-  constructor() { }
+export class DonationHomeComponent implements OnInit {
+  donations: any = [];
+
+  constructor(private donationsService: DonationsService) {}
 
   ngOnInit() {
+    this.donationsService.getDonations().subscribe(data => {
+      this.donations = data;
+    });
   }
-
 }
