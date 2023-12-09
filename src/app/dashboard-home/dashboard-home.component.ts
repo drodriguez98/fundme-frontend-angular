aa6d591f408684fe21d1c1044067be1f801ab115
Diego Rodriguez <diego25598@gmail.com>
2023-12-09 04:30:11 +0100

    update dashboard component

---
+++ src/app/dashboard-home/dashboard-home.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ProjectsService } from '../projects.service';
+import { DonationsService } from '../donations.service';
+
+@Component({
+  selector: 'app-dashboard-home',
+  templateUrl: './dashboard-home.component.html',
+  styleUrls: ['./dashboard-home.component.css']
+})
+export class DashboardHomeComponent implements OnInit {
+
+  projects: any = [];
+  donations: any = [];
+
+  constructor(private projectsService: ProjectsService, private donationsService: DonationsService) {}
+
+  ngOnInit() {
+    this.projectsService.getTopProjects().subscribe(data => {
+      this.projects = data;
+    });
+    this.donationsService.getTopDonations().subscribe(data => {
+      this.donations = data;
+    });
+  }
+}
+
