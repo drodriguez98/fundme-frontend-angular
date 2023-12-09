8c616b6346c3ce1f87cc699da34af91340e3088f
Diego Rodriguez <diego25598@gmail.com>
2023-12-09 04:28:01 +0100

    add application routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProjectHomeComponent } from './project-home/project-home.component';
+import { DonationHomeComponent } from './donation-home/donation-home.component';
+import { DashboardHomeComponent } from './dashboard-home/dashboard-home.component';
 
 
 const routes: Routes = [
 
-  { path: 'projects', component: ProjectHomeComponent }
+  { path: 'dashboard', component: DashboardHomeComponent },
+  { path: 'projects', component: ProjectHomeComponent },
+  { path: 'donations', component: DonationHomeComponent }
 
 ];
 
