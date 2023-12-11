7d797dbe9c433b40a1b088ea3f876554e92d0a99
Diego Rodriguez <diego25598@gmail.com>
2023-12-11 17:08:29 +0100

    add application modules and http interceptor to add jwt in headers requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProjectHomeComponent } from './project-home/project-home.component';
-import { MatButtonModule, MatIconModule, MatTableModule, MatToolbarModule } from '@angular/material';
-import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule, MatCardModule, MatDialogModule, MatFormFieldModule, MatIconModule, MatInputModule, MatTableModule, MatToolbarModule } from '@angular/material';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { DonationHomeComponent } from './donation-home/donation-home.component';
 import { DashboardHomeComponent } from './dashboard-home/dashboard-home.component';
+import { LoginHomeComponent } from './login-home/login-home.component';
+import { FormsModule } from '@angular/forms';
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProjectHomeComponent,
     DonationHomeComponent,
-    DashboardHomeComponent
+    DashboardHomeComponent,
+    LoginHomeComponent,
+    ProjectDetailComponent
   ],
   imports: [
     BrowserModule,
@@ -24,10 +29,18 @@ import { DashboardHomeComponent } from './dashboard-home/dashboard-home.componen
     MatTableModule,
     HttpClientModule,
     MatToolbarModule,
+    MatCardModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    FormsModule,
+    MatDialogModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
