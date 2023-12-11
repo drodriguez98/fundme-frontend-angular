8b860438c132db8265ae11462695264acb882d34
Diego Rodriguez <diego25598@gmail.com>
2023-12-11 17:07:39 +0100

    add public and private routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,21 +3,47 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProjectHomeComponent } from './project-home/project-home.component';
 import { DonationHomeComponent } from './donation-home/donation-home.component';
 import { DashboardHomeComponent } from './dashboard-home/dashboard-home.component';
-
+import { LoginHomeComponent } from './login-home/login-home.component';
+import { AuthGuard } from './auth/auth.guard'; // Importa el AuthGuard
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
 
 const routes: Routes = [
-
-  { path: 'dashboard', component: DashboardHomeComponent },
-  { path: 'projects', component: ProjectHomeComponent },
-  { path: 'donations', component: DonationHomeComponent }
-
+  // Rutas públicas
+  { path: 'login', component: LoginHomeComponent },
+  // Rutas protegidas por AuthGuard
+  {
+    path: 'dashboard',
+    component: DashboardHomeComponent,
+    canActivate: [AuthGuard] // Protege la ruta con AuthGuard
+  },
+  {
+    path: 'projects',
+    component: ProjectHomeComponent,
+    canActivate: [AuthGuard] // Protege la ruta con AuthGuard
+  },
+  {
+    path: 'project/:id',
+    component: ProjectDetailComponent,
+    canActivate: [AuthGuard] // Protege la ruta con AuthGuard
+  },
+  {
+    path: 'donations',
+    component: DonationHomeComponent,
+    canActivate: [AuthGuard] // Protege la ruta con AuthGuard
+  },
+  // Ruta predeterminada: redirige a dashboard si está autenticado, de lo contrario a login
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full',
+    canActivate: [AuthGuard] // Protege la ruta con AuthGuard
+  },
+  // Ruta de registro u otras rutas públicas
 ];
 
 @NgModule({
-
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
-  
 })
 
-export class AppRoutingModule { }
+export class AppRoutingModule {}
