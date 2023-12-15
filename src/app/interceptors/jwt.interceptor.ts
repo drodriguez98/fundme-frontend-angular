9aa7e5208d2db2deb802de8fa7f3dc05b98de1b9
Diego Rodriguez <diego25598@gmail.com>
2023-12-15 17:11:17 +0100

    format code

--- src/app/interceptors/jwt.interceptor.ts
+++ src/app/interceptors/jwt.interceptor.ts
@@ -1,28 +1,34 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
-} from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
     const authToken = localStorage.getItem('authToken');
 
     if (authToken) {
+
       // Concatenar "Bearer" al token antes de asignarlo al encabezado Authorization
+
       const authHeaderValue = `Bearer ${authToken}`;
+
       request = request.clone({
+
         setHeaders: {
+
           Authorization: authHeaderValue
+
         }
+
       });
+
     }
 
     return next.handle(request);
+
   }
+
 }
